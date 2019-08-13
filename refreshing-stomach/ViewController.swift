56dacf92f818dfe86b50393a209e3b433ac50e1b
lindelea <kgo@lindelin.org>
2019-08-13 11:27:32 +0900

    Initial Commit

---
+++ refreshing-stomach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  refreshing-stomach
+//
+//  Created by Jie Wu on 2019/08/13.
+//  Copyright © 2019 Lindelin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
